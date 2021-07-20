3f24d51efdc474769d29e9ddb734698344ede69b
PatrykBuniX <patrykbunix@gmail.com>
2021-07-20 09:50:33 +0200

    Display song's current time and duration above progress bar.

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -1,4 +1,5 @@
-import { MouseEvent, useEffect, useState } from "react";
+import { MouseEvent } from "react";
+import { createTimeStamp } from "../../utils/createTimeStamp";
 import styles from "./ProgressBar.module.scss";
 
 type Props = {
@@ -10,10 +11,12 @@ type Props = {
 export const ProgressBar = ({ currentTime, duration, handleClick }: Props) => {
   return (
     <div onClick={handleClick} className={styles.progressBar}>
+      <span className={styles.currentTime}>{createTimeStamp(currentTime)}</span>
       <div
         style={{ width: `${(100 * currentTime) / duration}%` }}
         className={styles.progress}
       ></div>
+      <span className={styles.duration}>{createTimeStamp(duration)}</span>
     </div>
   );
 };
